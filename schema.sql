65a5305a7082abb95c9a6625ddd24f41f7820a0f
Alexandra <Lexxiecole@gmail.com>
2018-10-22 18:08:56 -0400

    schema

---
+++ schema.sql
@@ -0,0 +1,38 @@
+CREATE DATABASE Bamazon;
+
+USE Bamazon;
+
+CREATE TABLE Products(
+    ItemID MEDIUMINIT AUTO_INCREMENT NOT NULL,
+    ProductName VARCHAR(100) NOT NULL,
+    DepartmentName VARCHAR(50) NOT NULL,
+    Price DECIMAL (10,2) NOT NULL,
+    StockQuantity INT (10) NOT NULL,
+    primary key(ItemID)
+);
+
+select * from Products;
+
+INSERT INTO Products(ProductName, DepartmentName, Price, StockQuantity)
+VALUES ("Hair tie", "beauty", 2.00, 1000),
+    ("Shirt", "clothing", 20.00, 1000),
+    ("flowers", "flowers", 6.00, 20),
+
+CREATE TABLE Departments(
+    DepartmentID MEDIUMINT AUTO_INCREMENT NOT NULL,
+    DepartmentName VARCHAR(50) NOT NULL,
+    OverHeadCosts DECIMAL(10,2) NOT NULL,
+    TotalSales DECIMAL(10,2) NOT NULL,
+    PRIMARY KEY(DepartmentID);
+
+INSERT INTO Departments(DepartmentName, OverHeadCosts, TotalSales)
+VALUES ('ENTERTAINMENT', 100000.00, 27000.00),
+    ('GROCERY', 15000.00, 11000.00),
+    ('CLOTHING', 55000.00, 21000.00),
+    ('HOME', 40000.00, 29000.00),
+    ('SPORTING GOODS', 18000.00, 10000.00),
+    ('HEALTH & BODY', 7500.00, 3900.00),
+    ('ELECTRONICS', 69000.00, 41000.00),
+    ('CHILDREN', 50000.00, 37000.00);
+)
+
